a2a57d514b852fbeb5bd85fbef7c828701605131
ssan9 <perrsy@gmail.com>
2021-05-22 22:32:13 -0400

    sorted articles using class method

--- src/App.js
+++ src/App.js
@@ -6,20 +6,50 @@ import Articles from './components/Articles';
 
 const title = "Sorting Articles";
 
-function App({articles}) {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            display: []
+        }
+        this.upvotedSort = this.upvotedSort.bind(this);
+        this.datedSort = this.datedSort.bind(this);
+    }
 
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
+    componentDidMount() {
+        this.setState({ 
+            display: this.props.articles.sort((a, b) => b.upvotes - a.upvotes) 
+        });
+    }
+
+
+    upvotedSort() {
+        this.state.display.sort((a, b) => b.upvotes - a.upvotes);
+        this.setState({
+            display: this.state.display
+        })
+    }
+
+    datedSort() {
+        this.state.display.sort((a, b) => new Date(b.date) - new Date(a.date));
+        this.setState({
+            display: this.state.display
+        });
+    }
+
+    render () {
+        return (
+            <div className="App">
+                <h8k-navbar header={title}></h8k-navbar>
+                <div className="layout-row align-items-center justify-content-center my-20 navigation">
+                    <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
+                    <button data-testid="most-upvoted-link" className="small" onClick={this.upvotedSort}>Most Upvoted</button>
+                    <button data-testid="most-recent-link" className="small" onClick={this.datedSort}>Most Recent</button>
+                </div>
+                <Articles articles={this.state.display}/>
             </div>
-            <Articles articles={articles}/>
-        </div>
-    );
-
+        );
+    }
 }
 
 export default App;
